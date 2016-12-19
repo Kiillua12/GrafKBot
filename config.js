1e1fd9de68d6caded4f1358d02752c2869aadb14
Mickael Jean <mickael@Andeol.local>
2016-12-19 16:53:21 +0100

    Ajout d'un fichier default et deleted du config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-var config = {};
-
-config.token = "MjYwNDE4NDQ3MzE2NDg0MTA3.CzmNvg.izKVz5KeDTuIotJKflWdZ0fJuO8";
-
-module.exports = config;
---
+++ configDefault.js
@@ -0,0 +1,5 @@
+var config = {};
+
+config.token = "MjU5Njk0MTI3MTQxMjI0NDQ4.CzmRhw.n5VZiTnEJPlkgVP4A7dWk8NfNWU";
+
+module.exports = config;
