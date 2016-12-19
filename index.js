a8b9790f3e0631020588f317171389c9cad620a8
Tigrouw <lucas.francoise.mp@gmail.com>
2016-12-19 15:39:04 +0100

    add reponse to coucou

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ var config = require('./config.js');
 var bot = new Discord.Client();
 
 bot.on("message", msg => {
-    if (msg.content.startsWith("ping")) {
-        msg.channel.sendMessage("pong!");
+    if (msg.content.startsWith("ping"))
+      msg.channel.sendMessage("pong!");
+    else if (msg.content.startsWith("coucou"))
+      msg.channel.sendMessage("toi meme")
+
+
     }
 });
 
