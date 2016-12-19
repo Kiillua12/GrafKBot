973ceeca3932724ebe4abb2847c6200da06943a4
Tigrouw <lucas.francoise.mp@gmail.com>
2016-12-19 16:39:30 +0100

    nouveau token de choup

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 var config = {};
 
-config.token = "MjU5Njk0MTI3MTQxMjI0NDQ4.Czlyxg.YzzCm-ixYkAxOqWHmsiNOe_J1-o";
+config.token = "MjYwNDE4NDQ3MzE2NDg0MTA3.CzmNvg.izKVz5KeDTuIotJKflWdZ0fJuO8";
 
 module.exports = config;
