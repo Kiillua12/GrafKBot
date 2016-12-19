da21f2ec8d148c37f7798b0e888162b0d3f21e34
Tigrouw <lucas.francoise.mp@gmail.com>
2016-12-19 17:29:06 +0100

    ajout DL de video

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
 var Discord = require("discord.js");
-var Ytdl = require("ytdl-core");
+
+/* File System */
+var fs = require('fs');
+
+/* Youtube Downloader */
+var ytdl = require("ytdl-core");
 var config = require('./config.js');
 var bot = new Discord.Client();
+
+bot.on('ready', () => {
+  console.log('I am ready!');
+});
 
 bot.on("message", msg => {
     if (msg.content.startsWith("ping"))
       msg.channel.sendMessage("pong!");
     else if (msg.content.startsWith("coucou"))
       msg.channel.sendMessage("toi meme");
-});
-
-bot.on('ready', () => {
-  console.log('I am ready!');
+    else if (msg.content.startsWith("ytdl")){
+      ytdl('https://www.youtube.com/watch?v=6Un9HLDCTCs', {
+        filter: function(format){
+          format.container === "mp4";
+          return format;
+        }
+      })
+      .pipe(fs.createWriteStream('video.mp4'));
+    }
 });
 
 bot.login(config.token);
-console.log(bot.user);
